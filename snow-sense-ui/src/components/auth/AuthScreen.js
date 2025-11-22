920f45d60e76f687c040d1d6f9c9a78e8b1ea49e
RohanRajendraDalvi <78230506+RohanRajendraDalvi@users.noreply.github.com>
2025-11-22 13:36:45 -0500

    Added  Gitignore

--- snow-sense-ui/src/components/auth/AuthScreen.js
+++ snow-sense-ui/src/components/auth/AuthScreen.js
@@ -1,3 +1,4 @@
+
 import React, { useCallback, useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { authStyles } from '../../styles/authStyles';
